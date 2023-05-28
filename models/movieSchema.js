42adac6f47a9f22bea13893fecbbfd9e6f44f755
Vladislav K <aljkeee@gmail.com>
2023-05-28 18:48:32 +0300

    Добавляет схемы пользователей и фильмов

--- models/movieSchema.js
+++ models/movieSchema.js
@@ -1,24 +1,68 @@
 const { Schema, model } = require('mongoose');
 const validator = require('validator');
 
-const movieSchema = new Schema(
-  {
-    country: {
-      type: String,
-      required: true,
-    },
-    director: {},
-    duration: {},
-    year: {},
-    description: {},
-    image: {},
-    trailerLink: {},
-    thumbnail: {},
-    owner: {},
-    movieId: {},
-    nameRU: {},
-    nameEN: {},
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    required: true,
   },
-);
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+    required: true,
+  },
+  trailerLink: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = model('movie', movieSchema);
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,29 +1,27 @@
 const { Schema, model } = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      validate: {
-        validator: (v) => validator.isEmail(v),
-        message: 'Некорректный email',
-      },
-      required: true,
-      unique: true,
+const userSchema = new Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Некорректный email',
     },
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
-    name: {
-      type: String,
-      required: true,
-      minlength: [2, 'Минимальная длина поля "name" - 2'],
-      maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    },
+    required: true,
+    unique: true,
   },
-);
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+});
 
 module.exports = model('user', userSchema);
